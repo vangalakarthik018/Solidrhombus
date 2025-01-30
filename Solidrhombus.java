255a0ac3d379f6fb19ec88441ef4a1337becab84
vangalakarthik018 <vangalakarthik018@gmail.com>
2025-01-30 19:46:40 +0530

    Add files via upload

---
+++ Solidrhombus.java
@@ -0,0 +1,15 @@
+public class Solidrhombus {
+    public static void main(String[] args) {
+        
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=5;j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+    
+}
